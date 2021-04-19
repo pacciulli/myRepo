ed4795e5a47f693759e71c1afb0a2c2202555e47
pacciulli <meyer.caue@gmail.com>
2021-04-19 16:42:41 +0900

    Organization of the folder

--- Python_Course/expense_tracker/restAPI/tests.py
+++ Python_Course/expense_tracker/restAPI/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from unittest import TestCase
+
+
+def two_integers_sum(a, b):
+    return 0
+
 
 # Create your tests here.
+class TestSum(TestCase):
+    def test_sum(self):
+        self.assertEqual(two_integers_sum(2, 1), 3)
