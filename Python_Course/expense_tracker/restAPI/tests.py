11ecc3e2f547db8aad965538ae92f00321cec37f
pacciulli <meyer.caue@gmail.com>
2021-04-20 09:50:01 +0900

    Addin model Expense

--- Python_Course/expense_tracker/restAPI/tests.py
+++ Python_Course/expense_tracker/restAPI/tests.py
@@ -1,12 +1,19 @@
 from django.test import TestCase
-from unittest import TestCase
-
-
-def two_integers_sum(a, b):
-    return a + b
+from restAPI import models
 
 
 # Create your tests here.
-class TestSum(TestCase):
-    def test_sum(self):
-        self.assertEqual(two_integers_sum(2, 3), 5)
+class TestModel(TestCase):
+    def test_expense(self):
+        expense = models.Expense.objects.create(
+            amount=249.99,
+            merchant="amazon",
+            description="anc headphones",
+            category="music",
+        )
+        inserted_expense = models.Expense.objects.get(pk=expense.id)
+
+        self.assertEqual(inserted_expense.amount, 249.99)
+        self.assertEqual(inserted_expense.merchant, "amazon")
+        self.assertEqual(inserted_expense.description, "anc headphones")
+        self.assertEqual(inserted_expense.category, "music")
